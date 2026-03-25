08cd9c5b9df127c6b49e33d5445ce880a4868275
xly116366-netizen <xly116366@gmail.com>
2026-03-25 10:28:59 +0800

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  use: {
+    baseURL: 'http://localhost:3000',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  reporter: [['list'], ['html', { open: 'never' }]],
+});
